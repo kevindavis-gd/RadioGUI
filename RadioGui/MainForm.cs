ac49862450612af69f5c9d21ff5becd3491d42ce
Kevin Davis <45161204+kevindavis-gd@users.noreply.github.com>
2020-09-10 16:18:23 -0500

    Add files via upload

---
+++ RadioGui/MainForm.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// Name: Kevin Davis
+/// Project: Radio GUI
+/// Class: CMPS 4143
+/// Date: 08/30/2020
+/// Description: A simple GUI Design of a Radio Interface taken from the C# text book.
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioGui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RadioGui/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Name: Kevin Davis
+/// Project: Radio GUI
+/// Class: CMPS 4143
+/// Date: 08/30/2020
+/// Description: A simple GUI Design of a Radio Interface taken from the C# text book.
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioGui
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
